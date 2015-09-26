478d7cdf7ecec1fced1f01ac387e7cde8ccead88
abs <archanabs15@gmail.com>
2015-09-26 22:29:00 +0530

    cleaned 4.py

--- one/4.py
+++ one/4.py
@@ -9,9 +9,6 @@ std_freq = {
     'Z':0.074 }
 eng_text = string.letters + string.digits + string.whitespace + ". ' ! ?"
 
-class SingleXorException(Exception):
-    pass
-
 class SingleXor(object):
     def __init__(self, cipher):
         self.cipher = cipher.decode('hex')
@@ -23,29 +20,29 @@ class SingleXor(object):
             temp = ''.join([chr(ord(i) ^ key) for i in self.cipher])
             if all(i in string.printable for i in temp):
                     printable_strings.append(temp)
+        # find freq difference with standard values
+        for each_string in printable_strings:
+            freq_table = {i : 0 for i in string.uppercase}
+            length = float(len(each_string))
+            each_string = filter(lambda x:x in string.letters, \
+                                               each_string.upper())
+            for each in each_string:
+                    freq_table[each] += 1
+            freq_table = {i : freq_table[i]/length for i in freq_table}
+            freq_diff.append(sum([abs(std_freq[a] - freq_table[a]) \
+                                  for a in std_freq]))
         try:
-            # find freq difference with standard values
-            for each_string in printable_strings:
-                freq_table = {i : 0 for i in string.uppercase}
-                length = float(len(each_string))
-                each_string = filter(lambda x:x in string.letters, \
-                                                   each_string.upper())
-                for each in each_string:
-                        freq_table[each] += 1
-                freq_table = {i : freq_table[i]/length for i in freq_table}
-                freq_diff.append(sum([abs(std_freq[a] - freq_table[a]) \
-                                      for a in std_freq]))
             position = freq_diff.index(min(freq_diff))
             possible_data = printable_strings[position]
             if all(i in eng_text for i in possible_data):
                 print possible_data
         except:
-            raise SingleXorException("Frequency differences is empty")
+                raise ValueError
 
 lines = [line.strip() for line in open('set1_4.txt')]
 for each in lines:
     try:
         string_xor = SingleXor(each)
         string_xor.decrypt()
-    except SingleXorException, e:
+    except ValueError, e:
         pass
