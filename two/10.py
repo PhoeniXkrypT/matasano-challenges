f2436953c689921ae8d617bc16f019c2a2f62e79
abs <archanabs15@gmail.com>
2015-10-06 21:09:47 +0530

    cleaned 10.py

--- two/10.py
+++ two/10.py
@@ -6,11 +6,11 @@ def AES_CBC_decrypt(cipher, key, IV, blocksize):
     cipher_blocks = [cipher[i:i+blocksize] for i in \
                      xrange(0, len(cipher), blocksize)]
     decrypted_blocks=[]
-    prev_block = IV
+    prev_block = IV.encode('hex')
     for current_block in cipher_blocks:
-        decrypted_blocks.append(util.fixed_xor(util.AES_ECB_decrypt(current_block, key).encode('hex'), \
-                                prev_block.encode('hex')).decode('hex'))
-        prev_block = current_block
+        temp = util.AES_ECB_decrypt(current_block, key).encode('hex')
+        decrypted_blocks.append(util.fixed_xor(temp, prev_block).decode('hex'))
+        prev_block = current_block.encode('hex')
     return ''.join(decrypted_blocks)
 
 lines = ''.join([line.strip() for line in open('set2_10.txt')])
