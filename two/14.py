8dbfdf402174465771c1c105f0cfefc87022a45d
abs <archanabs15@gmail.com>
2015-10-11 02:31:22 +0530

    cleaned 14.py

--- two/14.py
+++ two/14.py
@@ -10,8 +10,8 @@ get_random_string = lambda l : ''.join([chr(random.randint(0,255)) for i in xran
 AES_KEY = '.Rm\x10o\xaae\xf3coy}\xbf\x00\xa4&'
 
 def _encryption_oracle(message, blocksize=16):
-    pre = '\x82;<\xf1\xa6\xbeO\x02'
-    target = 'RGlzY292ZXIgdGhlIEJ5dGVzICE='
+    pre = 'c\xb3\xd1\xc8CL\x0e\xf0+\xe8'
+    target = 'VHdvIHJvYWRzIGRpdmVyZ2VkIGluIGEgd29vZCwgYW5kIEktCkkgdG9vayB0aGUgb25lIGxlc3MgdHJhdmVsZWQgYnks'
     modified_message = utils.pkcs7_padding(pre + message + base64.b64decode(target), blocksize)
     return util.AES_ECB_encrypt(modified_message, AES_KEY)
 
@@ -24,17 +24,18 @@ def find_prefix_length():
                 return (48 - i), k
     return None
 
-_len = 32
+_len = 128
+decoded_string = ""
 pre_len, pre_blocks = find_prefix_length()
 pad_len = 16 - pre_len
-decoded_string = ""
 for index in xrange(_len-1, 0, -1):
     inputs_dict = {}
     test_input = 'A' * (index + pad_len)
     for i in xrange(256):
-        inputs_dict[_encryption_oracle(test_input + decoded_string + chr(i))[pre_blocks*16:_len+pad_len]] = chr(i)
+        inputs_dict[_encryption_oracle(test_input + decoded_string + chr(i))[pre_blocks*16 : _len+pad_len]] = chr(i)
     try:
-        decoded_string += inputs_dict[_encryption_oracle(test_input)[pre_blocks*16:_len+pad_len]]
+        decoded_string += inputs_dict[_encryption_oracle(test_input)[pre_blocks*16 : _len+pad_len]]
     except KeyError,e :
         pass
-print decoded_string
+
+assert decoded_string == "Two roads diverged in a wood, and I-\nI took the one less traveled by,\x01"
