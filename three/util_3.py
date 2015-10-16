89687367b6e6b8a71fe080c173d09aca79d42281
abs <archanabs15@gmail.com>
2015-10-16 23:22:03 +0530

    added util_3.py

--- three/util_3.py
+++ three/util_3.py
@@ -1,4 +1,5 @@
 import sys
+import struct
 import base64
 import random
 from Crypto.Cipher import AES
@@ -54,4 +55,20 @@ def CBC_padding_oracle():
     else :
         return 404
 
-assert CBC_padding_oracle() == 200
+def ctr_stream_decrypt(cipher, key, blocksize=16):
+    nonce, counter = 0, 0
+    message = ""
+
+    cipher_blocks = [cipher[i:i+blocksize] for i in xrange(0,len(cipher), blocksize)]
+    for each in cipher_blocks:
+        intermediate = util_1.AES_ECB_encrypt(struct.pack("<Q", nonce) + struct.pack("<Q", counter), key)[:len(each)]
+        message += util_1.fixed_xor(each.encode('hex'), intermediate.encode('hex')).decode('hex')
+        counter += 1
+    return message
+
+
+def main():
+    if sys.argv[1] == "17":
+        assert CBC_padding_oracle() == 200
+    elif sys.argv[1] == "18":
+        assert ctr_stream_decrypt(base64.b64decode('L77na/nrFsKvynd6HzOoG7GHTLXsTVu9qvY/2syLXzhPw    eyyMTJULu/6/kXX0KSvoOLSFQ=='), 'YELLOW SUBMARINE') == "Yo, VIP Let's kick it Ice, Ice, baby Ice, Ice, baby "
