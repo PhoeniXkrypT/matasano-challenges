2cfb98d40a8a9cf1d4fd62cd642f4c30ab85742a
abs <archanabs15@gmail.com>
2015-10-17 00:23:24 +0530

    cleaned 18.py

--- three/18.py
+++ three/18.py
@@ -3,17 +3,16 @@ import base64
 
 import util_1
 
-def ctr_stream_decrypt(cipher, key, blocksize=16):
-    nonce, counter = 0, 0
-    message = ""
+def ctr_stream(input_string, key, nonce=0, blocksize=16):
+    counter = 0
+    out_message = ""
 
-    cipher_blocks = [cipher[i:i+blocksize] for i in xrange(0,len(cipher), blocksize)]
-    for each in cipher_blocks:
+    blocks = [input_string[i:i+blocksize] for i in xrange(0, len(input_string), blocksize)]
+    for each in blocks:
         intermediate = util_1.AES_ECB_encrypt(struct.pack("<Q", nonce) + struct.pack("<Q", counter), key)[:len(each)]
-        message += util_1.fixed_xor(each.encode('hex'), intermediate.encode('hex')).decode('hex')
+        out_message += util_1.fixed_xor(each.encode('hex'), intermediate.encode('hex')).decode('hex')
         counter += 1
-    return message
+    return out_message
 
-assert ctr_stream_decrypt(base64.b64decode('L77na/nrFsKvynd6HzOoG7GHTLXsTVu9qvY/2syLXzhPw    eyyMTJULu/6/kXX0KSvoOLSFQ=='), 'YELLOW SUBMARINE') == "Yo, VIP Let's kick it Ice, Ice, baby Ice, Ice, baby "
-
+assert ctr_stream(base64.b64decode('L77na/nrFsKvynd6HzOoG7GHTLXsTVu9qvY/2syLXzhPw    eyyMTJULu/6/kXX0KSvoOLSFQ=='), 'YELLOW SUBMARINE') == "Yo, VIP Let's kick it Ice, Ice, baby Ice, Ice, baby "
 
