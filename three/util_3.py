cca132b5d6275dcf37b58743b1159797b8f63fdb
abs <archanabs15@gmail.com>
2015-10-20 23:17:24 +0530

    added solution set3-23 to util_3

--- three/util_3.py
+++ three/util_3.py
@@ -123,6 +123,49 @@ def gen_rng_unix():
     rng = MT19937(seed)
     time.sleep(random.randint(4,15))
     return rng.extract_number(), seed
+    
+def clone_mt19937():
+    def reverse_right_shift(value, shift, mult=0xffffffff):
+        output, i = 0, 0
+        while i * shift < 32:
+            compartment = int(bin(0xffffffff << (32 - shift))[-32:], 2) >> (shift * i)
+            part_output = value & compartment
+            value ^= (part_output >> shift) & mult
+            output |= part_output
+            i += 1
+        return output
+
+    def reverse_left_shift(value, shift, mult=0xffffffff):
+        output, i = 0, 0
+        while i * shift < 32:
+            compartment = int(bin((0xffffffff >> (32- shift)) << (shift * i))[-32:], 2)
+            part_output = value & compartment
+            value ^= (part_output << shift) & mult
+            output |= part_output
+            i += 1
+        return output
+
+    def untemper(value):
+        value = reverse_right_shift(value, 18)
+        value = reverse_left_shift(value, 15, 4022730752)
+        value = reverse_left_shift(value, 7, 2636928640)
+        value = reverse_right_shift(value, 11)
+        return value
+
+    def temper(value):
+        value = value ^ value >> 11
+        value = value ^ value << 7 & 2636928640
+        value = value ^ value << 15 & 4022730752
+        value = value ^ value >> 18
+        return value
+
+    mt = MT19937(15015)
+    rand_numbers, states = [], []
+    for i in xrange(624):
+        rand_numbers.append(mt.extract_number())
+    for each in rand_numbers:
+        states.append(untemper(each))
+    return [1 for i in xrange(len(states)) if temper(states[i]) != rand_numbers[i]]
 
 def main():
     if sys.argv[1] == "17":
@@ -135,6 +178,8 @@ def main():
     elif sys.argv[1] == "22":
         number, seed = gen_rng_unix()
         assert crack_seed(number) == seed
+    elif sys.argv[1] == "23":
+        assert clone_mt19937() == []
 
 if __name__ == '__main__':
     main()
