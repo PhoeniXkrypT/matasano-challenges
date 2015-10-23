c12231a7a98280171a941e95012b91c2b05849df
abs <archanabs15@gmail.com>
2015-10-23 21:18:05 +0530

    added 24.py

--- three/24.py
+++ three/24.py
@@ -9,21 +9,20 @@ def mt19937_encrypt(message, seed):
 def recover_key(data, cipher):
     return [each for each in xrange(32768, 65535) if (mt19937_encrypt(data, each)) == cipher][0]
 
-def generate_password_token(seed):
+def password_token_generation(seed):
     mt = util_3.MT19937(seed)
     return mt.extract_number()
 
 def crack_password_token(token):
     current_time = int(time.time())
     for i in xrange(50):
-        mt = util_3.MT19937(current_time-i)
-        if mt.extract_number() == token:
+        mt = util_3.MT19937(current_time - i)
+        if token == mt.extract_number():
             return current_time - i
 
 data = ''.join([chr(random.randint(1,255)) for _ in xrange(random.randint(3,16))]) + ('A' * 14)
 seed = random.randint(32768,65535)
-cipher = mt19937_encrypt(data,seed)
-assert recover_key(data, cipher) == seed
-seed = int(time.time())
-token = generate_password_token(seed)
-assert crack_password_token(token) == seed
+assert recover_key(data, mt19937_encrypt(data, seed)) == seed
+time_seed = int(time.time())
+token = password_token_generation(time_seed)
+assert crack_password_token(token) == time_seed
