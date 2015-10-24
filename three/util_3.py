8591b02373640ad944f6c5bfed83f0b1673bea0a
abs <archanabs15@gmail.com>
2015-10-24 12:18:42 +0530

    added 24.py to util_3.py

--- three/util_3.py
+++ three/util_3.py
@@ -167,6 +167,24 @@ def clone_mt19937():
         states.append(untemper(each))
     return [1 for i in xrange(len(states)) if temper(states[i]) != rand_numbers[i]]
 
+def mt19937_encrypt(message, seed):
+    mt = MT19937(seed)
+    return ''.join([chr(ord(each) ^ (mt.extract_number() & 0xff)) for each in message])
+
+def recover_key(data, cipher):
+    return [each for each in xrange(32768, 65535) if (mt19937_encrypt(data, each)) == cipher][0]
+
+def password_token_generation(seed):
+    mt = MT19937(seed)
+    return mt.extract_number()
+
+def crack_password_token(token):
+    current_time = int(time.time())
+    for i in xrange(50):
+        mt = MT19937(current_time - i)
+        if token == mt.extract_number():
+            return current_time - i
+
 def main():
     if sys.argv[1] == "17":
         assert CBC_padding_oracle() == 200
@@ -180,6 +198,13 @@ def main():
         assert crack_seed(number) == seed
     elif sys.argv[1] == "23":
         assert clone_mt19937() == []
+    elif sys.argv[1] == "24":
+        data = ''.join([chr(random.randint(1,255)) for _ in xrange(random.randint(3,16))]) + ('A' * 14)
+        seed = random.randint(32768,65535)
+        assert recover_key(data, mt19937_encrypt(data, seed)) == seed
+        time_seed = int(time.time())
+        token = password_token_generation(time_seed)
+        assert crack_password_token(token) == time_seed
 
 if __name__ == '__main__':
     main()
