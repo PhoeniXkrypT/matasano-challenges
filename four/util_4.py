a0a27bd2f8002d5ee4d4536d4bb912f2646f1ff7
abs <archanabs15@gmail.com>
2015-10-24 23:43:30 +0530

    added solution 26 to util_4

--- four/util_4.py
+++ four/util_4.py
@@ -1,4 +1,5 @@
 import sys
+import util_2
 import util_3
 
 class ArgumentError(Exception):
@@ -14,12 +15,36 @@ def break_read_write_ctr(cipher):
     key = ''.join([chr(ord(i) ^ ord(j)) for i,j in zip(new_cipher, 'A' * len(cipher))])
     return ''.join([chr(ord(i) ^ ord(j)) for i,j in zip(cipher, key)])
 
+def ctr_bitflipping_attack():
+    AES_KEY = util_2.get_random_string(16)
+
+    def encrypt_modify(input_string):
+        pre = "comment1=cooking%20MCs;userdata="
+        post = ";comment2=%20like%20a%20pound%20of%20bacon"
+        if (';' in input_string) or ('=' in input_string):
+            input_string = input_string.replace(';', '').replace('=', '')
+        return util_3.ctr_stream(pre + input_string + post, AES_KEY)
+
+    def decrypt_search_admin(cipher):
+        message = util_3.ctr_stream(cipher, AES_KEY)
+        return (";admin=true;" in message)
+
+    cipher = encrypt_modify(" admin true")
+    for i in xrange(256):
+        for j in xrange(256):
+            new_cipher = cipher[:32] + chr(i) + cipher[33:38] + chr(j) + cipher[39:]
+            if decrypt_search_admin(new_cipher):
+                return True
+    return False
+
 def main():
     try:
         if sys.argv[1] == "25":
             plaintext = (''.join([line.strip() for line in open('out_7.txt')])).decode('hex')
             cipher = util_3.ctr_stream(plaintext, 'c62a824f5d01d4bca11d4382cddfca19'.decode('hex'))
             assert break_read_write_ctr(cipher) == plaintext
+        elif sys.argv[1] == "26":
+            assert ctr_bitflipping_attack() == True
         else:
             raise ArgumentError("Give argument between 25 and 32")
     except ArgumentError, e:
