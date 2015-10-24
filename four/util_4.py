3fbdbd4089220584494803ac06bc4ac3bf84d61b
abs <archanabs15@gmail.com>
2015-10-24 12:55:45 +0530

    renamed functions in util_4

--- four/util_4.py
+++ four/util_4.py
@@ -4,13 +4,13 @@ import util_3
 class ArgumentError(Exception):
     pass
 
-def edit(cipher, offset, newtext, key='\xc6*\x82O]\x01\xd4\xbc\xa1\x1dC\x82\xcd\xdf\xca\x19'):
+def edit_api(cipher, offset, newtext, key='c62a824f5d01d4bca11d4382cddfca19'.decode('hex')):
     plaintext = util_3.ctr_stream(cipher, key)
     new_plaintext = plaintext[:offset] + newtext + plaintext[offset:]
     return util_3.ctr_stream(new_plaintext, key)
 
-def retrive_plain(cipher):
-    new_cipher = edit(cipher, 0, 'A' * len(cipher))
+def break_read_write_ctr(cipher):
+    new_cipher = edit_api(cipher, 0, 'A' * len(cipher))
     key = ''.join([chr(ord(i) ^ ord(j)) for i,j in zip(new_cipher, 'A' * len(cipher))])
     return ''.join([chr(ord(i) ^ ord(j)) for i,j in zip(cipher, key)])
 
@@ -18,8 +18,8 @@ def main():
     try:
         if sys.argv[1] == "25":
             plaintext = (''.join([line.strip() for line in open('out_7.txt')])).decode('hex')
-            cipher = util_3.ctr_stream(plaintext, '\xc6*\x82O]\x01\xd4\xbc\xa1\x1dC\x82\xcd\xdf\xca\x19')
-            assert retrive_plain(cipher) == plaintext
+            cipher = util_3.ctr_stream(plaintext, 'c62a824f5d01d4bca11d4382cddfca19'.decode('hex'))
+            assert break_read_write_ctr(cipher) == plaintext
         else:
             raise ArgumentError("Give argument between 25 and 32")
     except ArgumentError, e:
