fb1c79019de52518885e6ddd183a3f0df27a1695
abs <archanabs15@gmail.com>
2015-10-24 12:28:38 +0530

    cleaned util_4

--- four/util_4.py
+++ four/util_4.py
@@ -1,4 +1,8 @@
+import sys
 import util_3
+
+class ArgumentError(Exception):
+    pass
 
 def edit(cipher, offset, newtext, key='\xc6*\x82O]\x01\xd4\xbc\xa1\x1dC\x82\xcd\xdf\xca\x19'):
     plaintext = util_3.ctr_stream(cipher, key)
@@ -10,8 +14,16 @@ def retrive_plain(cipher):
     key = ''.join([chr(ord(i) ^ ord(j)) for i,j in zip(new_cipher, 'A' * len(cipher))])
     return ''.join([chr(ord(i) ^ ord(j)) for i,j in zip(cipher, key)])
 
+def main():
+    try:
+        if sys.argv[1] == "25":
+            plaintext = (''.join([line.strip() for line in open('out_7.txt')])).decode('hex')
+            cipher = util_3.ctr_stream(plaintext, '\xc6*\x82O]\x01\xd4\xbc\xa1\x1dC\x82\xcd\xdf\xca\x19')
+            assert retrive_plain(cipher) == plaintext
+        else:
+            raise ArgumentError("Give argument between 25 and 32")
+    except ArgumentError, e:
+        print e
 
-plaintext = (''.join([line.strip() for line in open('out_7.txt')])).decode('hex')
-AES_KEY = '\xc6*\x82O]\x01\xd4\xbc\xa1\x1dC\x82\xcd\xdf\xca\x19'
-cipher = util_3.ctr_stream(plaintext, AES_KEY)
-assert retrive_plain(cipher) == plaintext
+if __name__ == '__main__':
+    main()
