627fbefaaa8d1c79aaef6d5c37430017af12b91d
abs <archanabs15@gmail.com>
2015-10-29 18:35:29 +0530

    modified challenge-27,28 solution in util_4

--- four/util_4.py
+++ four/util_4.py
@@ -46,14 +46,15 @@ def cbc_key_as_iv():
     key = util_2.get_random_string(blocksize)
 
     def recover_key(cipher):
-        new_cipher = cipher[:16] + '\x00'*blocksize + cipher[:16] + cipher[48:]
+        new_cipher = cipher[:16] + '\x00' * blocksize + cipher[:16] + cipher[48:]
         msg = util_2.pkcs7_unpadding(util_2.AES_CBC_decrypt(new_cipher, key, key, blocksize), blocksize)
-        return util_1.fixed_xor(msg[:16].encode('hex'), msg[32:48].encode('hex')).decode('hex')
+        if not(all(each in string.printable for each in msg)):
+            return util_1.fixed_xor(msg[:16].encode('hex'), msg[32:48].encode('hex')).decode('hex')
+        return 0
 
     message = "comment1=cooking%20MCs;userdata=testuser;comment2=%20like%20a%20pound%20of%20bacon"
     cipher = util_2.AES_CBC_encrypt(util_2.pkcs7_padding(message, blocksize), key, key, blocksize)
-    recovered_key = recover_key(cipher)
-    return (key == recovered_key)
+    return (key == recover_key(cipher))
 
 class SHA1(object):
     _h0, _h1, _h2, _h3, _h4, = (
