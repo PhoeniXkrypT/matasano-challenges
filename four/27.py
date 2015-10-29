1b8d980929f790d57f3042328e8abbf90a6f23f2
abs <archanabs15@gmail.com>
2015-10-29 18:29:24 +0530

    added out_7.txt

--- four/27.py
+++ four/27.py
@@ -2,6 +2,8 @@ import util_1
 import util_2
 import util_3
 
+import string
+
 def cbc_key_as_iv():
     blocksize = 16
     key = util_2.get_random_string(blocksize)
@@ -9,7 +11,9 @@ def cbc_key_as_iv():
     def recover_key(cipher):
         new_cipher = cipher[:16] + '\x00'*blocksize + cipher[:16] + cipher[48:]
         msg = util_2.pkcs7_unpadding(util_2.AES_CBC_decrypt(new_cipher, key, key, blocksize), blocksize)
-        return util_1.fixed_xor(msg[:16].encode('hex'), msg[32:48].encode('hex')).decode('hex')
+        if not(all(each in string.printable for each in msg)):
+            return util_1.fixed_xor(msg[:16].encode('hex'), msg[32:48].encode('hex')).decode('hex')
+        return 0
 
     message = "comment1=cooking%20MCs;userdata=testuser;comment2=%20like%20a%20pound%20of%20bacon"
     cipher = util_2.AES_CBC_encrypt(util_2.pkcs7_padding(message, blocksize), key, key, blocksize)
